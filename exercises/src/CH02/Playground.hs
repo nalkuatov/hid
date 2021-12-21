dcaa9b3a64984624224e4422978d2565159d42df
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-12-21 23:58:04 +0600

    Update chapter 2

--- exercises/src/CH02/Playground.hs
+++ exercises/src/CH02/Playground.hs
@@ -1,6 +1,12 @@
 module CH02.Playground where
 
 class (Enum a, Bounded a) => CyclicEnum a where
+  cpred :: a -> a
   csucc :: a -> a
 
-data Direction = North | South | East | West
+data Direction
+  = North
+  | South
+  | East
+  | West
+  deriving (Enum, Bounded)
