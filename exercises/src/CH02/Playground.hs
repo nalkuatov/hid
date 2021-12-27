d50bf81760971e8a9ccd8210a7e875a61dc6f7e6
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-12-27 22:54:13 +0600

    Update chapter 2

--- exercises/src/CH02/Playground.hs
+++ exercises/src/CH02/Playground.hs
@@ -1,12 +1,37 @@
 module CH02.Playground where
 
-class (Enum a, Bounded a) => CyclicEnum a where
-  cpred :: a -> a
-  csucc :: a -> a
+class (Eq a, Enum a, Bounded a) => CyclicEnum a where
 
-data Direction
-  = North
-  | South
-  | East
-  | West
-  deriving (Enum, Bounded)
+  cpred :: a -> a
+  cpred a
+    | a == minBound = maxBound
+    | otherwise     = pred a
+
+  csucc :: a -> a
+  csucc a
+    | a == maxBound = minBound
+    | otherwise     = succ a
+
+data Direction = North | East | South | West
+  deriving (Eq, Enum, Bounded, Show)
+
+data Turn = TNone | TLeft | TRight | TAround
+  deriving (Eq, Enum, Bounded, Show)
+
+rotate :: Turn -> Direction -> Direction
+rotate = undefined
+
+orient :: Direction -> Direction -> Turn
+orient = undefined
+
+rotateMany :: [Turn] -> Direction -> Direction
+rotateMany = undefined
+
+orientMany :: [Direction] -> [Turn]
+orientMany = undefined
+
+rotateFromFile :: Direction -> FilePath -> IO Direction
+rotateFromFile = undefined
+
+main :: IO ()
+main = undefined
