3af383ea0913229046627c6ed62081bc14208749
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2022-02-03 16:17:35 +0600

    Jump to chapter 11

---
+++ exercises/src/CH11/Playground.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module CH11.Playground where
+
+import Data.Kind
+import Data.Proxy
+
+newtype Temp unit = Temp Double deriving
+  (Num, Fractional)
+
+data F
+data C
+
+paperBurning :: Temp F
+paperBurning = 451
+
+zeroAbsolute :: Temp C
+zeroAbsolute = -273.15
+
+f2c :: Temp F -> Temp C
+f2c (Temp f) = Temp $ (f - 32) * 5 / 9
+
+nonsense :: Temp Bool
+nonsense = 0
+
+class UnitName (u :: Type) where
+  name :: Proxy u -> String
+
+instance UnitName F where
+  name :: Proxy F -> String
+  name _ = "F"
+
+instance UnitName C where
+  name :: Proxy C -> String
+  name _ = "C"
+
+instance UnitName unit => UnitName (Temp unit) where
+  name _ = name (Proxy :: Proxy unit)
+
+instance UnitName unit => Show (Temp unit) where
+  show (Temp t) = show t <> "°" <> name (Proxy :: Proxy unit)
+
+data K
+
+instance UnitName K where
+  name _ = "K"
+
+unit :: forall unit. UnitName unit => Temp unit -> String
+unit (Temp t) = name (Proxy :: Proxy unit)
