4c1c35c66900424bd307114f6cf1a3bb57f8aa51
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2022-02-16 23:56:13 +0600

    Update chapter 2

--- exercises/src/CH02/Person.hs
+++ exercises/src/CH02/Person.hs
@@ -1,1 +1,15 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module CH02.Person where
+
+import Data.String
+
+data Person = Person String (Maybe Int)
+
+deriving instance Show Person
+deriving instance Read Person
+deriving instance Eq Person
+
+instance IsString Person where
+  fromString name = Person name Nothing
