98f87c51278b4178dc8e116073f54bdc1e2c18e6
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2022-04-12 23:56:13 +0600

    Add dummy `hedgehog` tests

--- exercises/test/Spec.hs
+++ exercises/test/Spec.hs
@@ -1,2 +1,17 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE ImportQualifiedPost #-}
+
+import Hedgehog
+import Hedgehog.Range qualified as Range
+import Hedgehog.Gen   qualified as Gen
+
+import Control.Applicative
+
+sample_property :: Property
+sample_property = property $ do
+  (a, b) <- forAll $
+    liftA2 (,) (Gen.int32 $ Range.linear 1 10) (Gen.int32 $ Range.linear 1 10)
+  a + b === b + a
+
+main :: IO Bool
+main = checkParallel $
+  Group "sample" [("samples", sample_property)]
